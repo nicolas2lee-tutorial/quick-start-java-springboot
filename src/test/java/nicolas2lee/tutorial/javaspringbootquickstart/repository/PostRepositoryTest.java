b617c9e90c8acc350f50d2800c4cacd8643b68f5
taozh <nicolas2lee@gmail.com>
2022-08-15 16:56:03 +0200

    update jdk

--- src/test/java/nicolas2lee/tutorial/javaspringbootquickstart/repository/PostRepositoryTest.java
+++ src/test/java/nicolas2lee/tutorial/javaspringbootquickstart/repository/PostRepositoryTest.java
@@ -1,20 +1,19 @@
 package nicolas2lee.tutorial.javaspringbootquickstart.repository;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 @DataJpaTest
 class PostRepositoryTest {
-    @Autowired
-    private PostRepository postRepository;
+  @Autowired private PostRepository postRepository;
 
-    @Test
-    void should_return_2_posts() {
-        var entities = postRepository.findAll();
-        assertThat(entities).hasSize(2);
-        assertThat(entities.get(1).getContent()).isEqualTo("my second post");
-    }
+  @Test
+  void should_return_2_posts() {
+    var entities = postRepository.findAll();
+    assertThat(entities).hasSize(2);
+    assertThat(entities.get(1).getContent()).isEqualTo("my second post");
+  }
 }
